2ff95fa4ac1943b875198c84a855826d23de0d47
chenyunshu2 <chenyunshu2@gmail.com>
2023-12-27 20:52:31 +0800

    implement pagination

--- src/controllers/AuthorsController.ts
+++ src/controllers/AuthorsController.ts
@@ -2,15 +2,16 @@ import { AppDataSource } from "../database/data-source";
 import { Request, Response } from "express";
 import { Author } from "../entities/Author";
 import { ResponseUtil } from "../utils/Response";
+import { Paginator } from "../database/Paginator";
 
 export class AuthorsController {
     async getAuthors(req: Request, res: Response) {
-        const authors = await AppDataSource.getRepository(Author).find();
-        return res.status(200).json({
-            success: true,
-            message: 'Fetch authors successfully',
-            data: authors,
-        });
+        const builder = await AppDataSource
+            .getRepository(Author)
+            .createQueryBuilder()
+            .orderBy('id', 'DESC');
+        const { records: authors, paginationInfo } = await Paginator.paginate(builder, req);
+        return ResponseUtil.sendResponse(res, 'Fetched authors successfully', authors, paginationInfo)
     }
 
     async getAuthor(req: Request, res: Response) {
